54dadc05a8b22c35511e8c50d442640b89931387
MajharolTanvir <majharul5456@gmail.com>
2023-12-05 13:08:38 +0600

    Service section color changed

--- src/components/UI/Service/page.tsx
+++ src/components/UI/Service/page.tsx
@@ -4,35 +4,35 @@ import styles from "./service.module.css";
 const Service = () => {
   return (
     <div className="mt-20">
-      <div className="lg:grid grid-cols-12 items-center justify-items-center">
+      <div className="w-[90%] mx-auto">
         <div
-          className={`lg:-rotate-90 col-span-2 text-[50px] lg:text-[100px] xl:text-[130px] md:text-[80px] font-bold ${styles.header_title}`}
+          className={`text-[50px] lg:text-[100px] xl:text-[130px] md:text-[80px] font-bold ${styles.header_title}`}
         >
           <h1>Services</h1>
         </div>
-        <div className="col-span-10 mt-10 md:mt-0 w-[90%]">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 sm:gap-10 justify-start items-center">
+        <div className="mt-10 md:mt-0">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 sm:gap-10 justify-center items-center text-[#114b68]">
             <div
               className={`border border-secondaryColor p-5 md:py-10 flex flex-col justify-center items-center rounded-lg ${styles.service_Section}`}
             >
-              <FaCode className="text-5xl text-secondaryColor" />
-              <h3 className="text-lg md:text-xl lg:text-2xl font-medium text-secondaryColor">
+              <FaCode className="text-5xl" />
+              <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
                 Frontend Development
               </h3>
             </div>
             <div
               className={`border border-secondaryColor p-5 md:py-10 flex flex-col justify-center items-center rounded-lg ${styles.service_Section}`}
             >
-              <FaCode className="text-5xl text-secondaryColor" />
-              <h3 className="text-lg md:text-xl lg:text-2xl font-medium text-secondaryColor">
+              <FaCode className="text-5xl" />
+              <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
                 Backend Development
               </h3>
             </div>
             <div
               className={`border border-secondaryColor p-5 md:py-10 flex flex-col justify-center items-center rounded-lg ${styles.service_Section}`}
             >
-              <FaCode className="text-5xl text-secondaryColor" />
-              <h3 className="text-lg md:text-xl lg:text-2xl font-medium text-secondaryColor">
+              <FaCode className="text-5xl" />
+              <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
                 Full-stack Development
               </h3>
             </div>
@@ -40,24 +40,24 @@ const Service = () => {
             <div
               className={`border border-secondaryColor p-5 md:py-10 flex flex-col justify-center items-center rounded-lg ${styles.service_Section}`}
             >
-              <FaWordpress className="text-5xl text-secondaryColor" />
-              <h3 className="text-lg md:text-xl lg:text-2xl font-medium text-secondaryColor">
+              <FaWordpress className="text-5xl" />
+              <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
                 Wordpress Development
               </h3>
             </div>
             <div
               className={`border border-secondaryColor p-5 md:py-10 flex flex-col justify-center items-center rounded-lg ${styles.service_Section}`}
             >
-              <FaWix className="text-5xl text-secondaryColor" />
-              <h3 className="text-lg md:text-xl lg:text-2xl font-medium text-secondaryColor">
+              <FaWix className="text-5xl" />
+              <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
                 Wix Development
               </h3>
             </div>
             <div
               className={`border border-secondaryColor p-5 md:py-10 flex flex-col justify-center items-center rounded-lg ${styles.service_Section}`}
             >
-              <FaSquarespace className="text-5xl text-secondaryColor" />
-              <h3 className="text-lg md:text-xl lg:text-2xl font-medium text-secondaryColor">
+              <FaSquarespace className="text-5xl" />
+              <h3 className="text-lg md:text-xl lg:text-2xl font-medium">
                 Squarespace Development
               </h3>
             </div>
@@ -65,7 +65,7 @@ const Service = () => {
         </div>
       </div>
       <div className="flex justify-end items-center mt-16">
-        <button className="bg-secondaryColor/80 hover:bg-transparent hover:shadow-sm hover:shadow-secondaryColor text-mainColor hover:border rounded-md hover:border-secondaryColor/80 hover:text-secondaryColor px-5 py-1 text-white font-medium">
+        <button className="bg-secondaryColor/80 hover:bg-transparent hover:shadow-sm hover:shadow-secondaryColor text-mainColor hover:border rounded-md hover:border-secondaryColor/80 hover px-5 py-1 text-white font-medium">
           Show service details
         </button>
       </div>
