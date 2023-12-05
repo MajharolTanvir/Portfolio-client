b257706f689f4558ca3b13ee27cf64958c51e3e9
MajharolTanvir <majharul5456@gmail.com>
2023-12-05 15:28:26 +0600

    News letter  section color changed

--- src/components/UI/NewsLetter/newsLetter.tsx
+++ src/components/UI/NewsLetter/newsLetter.tsx
@@ -3,21 +3,21 @@ import React from "react";
 
 const NewsLetterSection = () => {
   return (
-    <div className="bg-secondaryColor/5 py-10 mt-20">
-      <div className="flex flex-col justify-center items-center ">
-        <h4 className="text-3xl font-bold text-secondaryColor">
+    <div className="bg-[#114b68] py-10 mt-20">
+      <div className="flex flex-col justify-center items-center text-[#e8f7fb]">
+        <h4 className="text-xl md:text-2xl lg:text-3xl font-bold">
           Don&apos;t miss to Subscribe
         </h4>
-        <p className="pt-2 pb-8 text-accentColor px-5 text-center">
+        <p className="pt-2 pb-8 px-5 text-center">
           You received important blogs notification when publish in here.
         </p>
-        <div className="relative">
+        <div className="flex w-60 md:w-80 shadow-md rounded-md">
           <Input
             type="text"
             placeholder="username@site.com"
-            className="input input-bordered w-full pr-16"
+            className="input input-bordered w-full"
           />
-          <button className="btn bg-secondaryColor text-mainColor absolute top-0 right-0 rounded-l-none h-full px-2 rounded">
+          <button className="btn bg-[#114b68] rounded-l-none h-full py-2 px-2 rounded">
             Subscribe
           </button>
         </div>
